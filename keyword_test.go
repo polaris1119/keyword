96ae6735f2f2ab2a3859ef65eb22ed53da502cd5
xuxinhua <xuxinhua@zhimadj.com>
2017-06-08 15:59:27 +0800

    自动提取关键词

---
+++ keyword_test.go
@@ -0,0 +1,13 @@
+// Copyright 2016 polaris. All rights reserved.
+// Use of l source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+// http://studygolang.com
+// Author: polaris	polaris@studygolang.com
+
+package keyword_test
+
+import "testing"
+
+func TestExtract(t *testing.T) {
+
+}
